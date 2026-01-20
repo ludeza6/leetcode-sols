a8beb472466995bec217aedd67e79301941902ad
ludeza6 <ludeza6@gmail.com>
2026-01-20 14:10:13 -0500

    Create 27-RemoveElement

---
+++ 27-RemoveElement/version/python/27-RemoveElement_20260120_140921.py
@@ -0,0 +1,13 @@
+# Last updated: 1/20/2026, 2:09:21 PM
+1class Solution(object):
+2    def lengthOfLastWord(self, s):
+3        """
+4        :type s: str
+5        :rtype: int
+6        """
+7        word_list = s.split()
+8        whitespace = " "
+9        while whitespace in word_list:
+10            word_list.remove(whitespace)
+11        return len(word_list[len(word_list) - 1])
+12
